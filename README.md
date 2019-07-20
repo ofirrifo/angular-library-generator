776c05bedcb8ac12ca620b5640d6b981e195c42e
ofirf <ofirf@sentinel.com>
2019-07-20 19:34:39 +0300

    first commit

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # angular-library-generator
-Angular Library Generator
+Angular Library Generator.
 
+# What is do
+- Create an angular workspace 
+- Create angular library
+- Create angular example application
+- Add MIT LICENSE file
+- Add prettier 
+- Add some package.json scripts 
+
+ 
 ## Installation
 ```sh
 npm i -g @rifo/angular-library-generator 
--- genAngularLibrary.js
+++ genAngularLibrary.js
@@ -6,6 +6,9 @@ const cmd = require('node-cmd');
 const readline = require('readline');
 const fs = require('fs');
 const chalk = require('chalk');
+const ora = require('ora');
+let spinner;
+
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -25,7 +28,7 @@ function createAngularLibrary(libName, fullName) {
 }
 
 function createAngularWorkspace(libName, fullName) {
-    logSteps(`- Creating Angular Workspace`);
+    logSteps(`Creating Angular Workspace`);
     cmd.get(
         `
     ng new ${libName} --create-application=false
@@ -35,7 +38,7 @@ function createAngularWorkspace(libName, fullName) {
 }
 
 function generateAngularLibrary(libName, fullName) {
-    logSteps(`- Creating Angular Library`);
+    logSteps(`Creating Angular Library`);
     cmd.get(
         `
         cd ${libName} 
@@ -45,7 +48,7 @@ function generateAngularLibrary(libName, fullName) {
 }
 
 function generateAngularApplicationExample(libName, fullName) {
-    logSteps(`- Creating Angular application example`);
+    logSteps(`Creating Angular application example`);
     cmd.get(
         `
         cd ${libName}
@@ -55,7 +58,7 @@ function generateAngularApplicationExample(libName, fullName) {
 }
 
 function installAdditionalNpmPackages(libName, fullName) {
-    logSteps(`- Installing additional npm packages: prettier, tslint-config-prettier`);
+    logSteps(`Installing additional npm packages: prettier, tslint-config-prettier`);
     cmd.get(
         `
         cd ${libName}
@@ -65,15 +68,9 @@ function installAdditionalNpmPackages(libName, fullName) {
         () => {
             createLicenseFile(libName, fullName);
             copyPrettierFile(libName);
-            console.log(chalk.hex('#1ec537').bold(`\n\r Library ${libName} created successfully.`));
+            console.log(chalk.hex('#1ec537').bold(`\n\r Library ${libName} created successfully. 💪`));
         }
     );
-}
-
-function copyPrettierFile(libName) {
-    const data = fs.readFileSync(`${__dirname}/template-files/.prettierrc`, 'utf-8');
-    fs.writeFileSync(`./${libName}/.prettierrc`, data, 'utf-8');
-    logSteps('- adding .prettierrc file.');
 }
 
 function createLicenseFile(libName, fullName) {
@@ -86,7 +83,14 @@ function createLicenseFile(libName, fullName) {
     let newValue = data.replace('[year]', new Date().getFullYear());
     newValue = newValue.replace('[fullname]', fullName);
     fs.writeFileSync(`./${libName}/LICENSE`, newValue, 'utf-8');
-    logSteps('- adding LICENSE file.');
+    logSteps('Adding LICENSE file.');
+}
+
+function copyPrettierFile(libName) {
+    const data = fs.readFileSync(`${__dirname}/template-files/.prettierrc`, 'utf-8');
+    fs.writeFileSync(`./${libName}/.prettierrc`, data, 'utf-8');
+    logSteps('Adding .prettierrc file.');
+    spinner.succeed();
 }
 
 function logBlue(text) {
@@ -98,11 +102,14 @@ function logYellow(text) {
 }
 
 function logSteps(text) {
-    console.log(chalk.greenBright(text));
+    if (spinner) {
+        spinner.succeed();
+    }
+    spinner = ora(`${chalk.hex('#98c379')(text)}`).start();
 }
 
 const main = async () => {
-    logBlue('Hi, welcome to Angular Library Generator \n\r');
+    logBlue('Hi, welcome to Angular Library Generator 🚀 \n\r');
     const libName = await question('Enter library name');
     const fullName = await question('Enter your full name');
     rl.close();
